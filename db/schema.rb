eb064dfb6de4841c1e96b63882f747dbca4f98ec
Rony <ronykocheril@gmail.com>
2017-11-13 05:08:09 +0000

    Province model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112175759) do
+ActiveRecord::Schema.define(version: 20171113050717) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 20171112175759) do
     t.string "EmailAddress"
     t.string "Image"
     t.text "Notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
